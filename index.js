05d20d1c3da272941876e56c9aaf49906e07d427
Mekhdi78 <132650071+Mekhdi78@users.noreply.github.com>
2023-07-01 17:29:39 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// // task 1   
+
+let btn = document.querySelector('button')
+
+function creatingCalculator() {
+
+    let number1 = +prompt('Enter first number: ')
+    let operator = prompt('Enter operator ( either +, -, * or / ): ')
+    let number2 = +prompt('Enter second number: ')
+
+    if (operator == '+') {
+        alert(number1 + number2)
+    }
+    else if (operator == '-') {
+        alert(number1 + number2)
+    }
+    else if (operator == '*') {
+        alert(number1 + number2)
+    }
+    else {
+        alert(number1 + number2)
+    }
+}
+
+btn.onclick = () => {
+    creatingCalculator()
+}
+
+// task 2
+
+let num = +prompt('общее кол-во')
+let num1 = +prompt('найти из')
+
+function findingPercent() {
+    let answer = (num1 / num) * 100
+    alert(answer);
+}
+findingPercent()
